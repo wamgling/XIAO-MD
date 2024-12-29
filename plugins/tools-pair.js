28e06e3cfeb050d9938f28605bb95d58a680fe8a
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-29 14:30:01 +0200

    Update tools-pair.js

--- plugins/tools-pair.js
+++ plugins/tools-pair.js
@@ -1,1 +1,41 @@
-
+const {
+  cmd,
+  commands
+} = require("../command");
+const {
+  getBuffer,
+  getGroupAdmins,
+  getRandom,
+  h2k,
+  isUrl,
+  Json,
+  sleep,
+  fetchJson
+} = require("../lib/functions");
+cmd({
+  'pattern': 'pair',
+  'alias': ["register", 'link'],
+  'react': '🔢',
+  'desc': "pair",
+  'category': 'tool',
+  'use': ".pair +263719647303",
+  'filename': __filename
+}, async (_0x5b297c, _0x3a3e44, _0xefafe8, {
+  from: _0x5aafb9,
+  prefix: _0x322641,
+  quoted: _0x587384,
+  q: _0x560654,
+  reply: _0x2b55a0
+}) => {
+  try {
+    if (!_0x560654) {
+      return await _0x2b55a0("*Example - :* .pair +263719647303");
+    }
+    const _0x29763b = await fetchJson("https://la-session-kerm-v4.onrender.com/pair/code?number=" + _0x560654);
+    const _0x4b5713 = _0x29763b.code;
+    _0xefafe8.reply(_0x4b5713 + "\n\n" + "_*𝗦𝗨𝗕𝗭𝗘𝗥𝗢-𝗠𝗗 NUMBER PAIRED SUCCESSFULLY...✅*_");
+  } catch (_0x5be2b9) {
+    console.log(_0x5be2b9);
+    _0x2b55a0(_0x5be2b9);
+  }
+});
