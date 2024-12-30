ed3a32ab0518b403a15cbf77d39b0af5b26fcbad
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-30 18:33:31 +0200

    Update about.js

--- plugins/about.js
+++ plugins/about.js
@@ -23,7 +23,7 @@ const config = require('../config');
 const {cmd , commands} = require('../command')
 cmd({
     pattern: "about",
-    alias: ["darrell"],
+    alias: ["mrfrank"],
     react: "🧠",
     desc: "get owner dec",
     category: "main",
