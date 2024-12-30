efc1322b69e8bbb2a92f6a12d75dec915f10f3df
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-30 11:16:26 +0200

    Update tools-pair.js

--- plugins/tools-pair.js
+++ plugins/tools-pair.js
@@ -19,7 +19,7 @@ cmd({
   'react': '🔢',
   'desc': "pair",
   'category': 'download',
-  'use': ".pair +923096287432",
+  'use': ".pair +263719647303",
   'filename': __filename
 }, async (_0x5b297c, _0x3a3e44, _0xefafe8, {
   from: _0x5aafb9,
@@ -30,11 +30,11 @@ cmd({
 }) => {
   try {
     if (!_0x560654) {
-      return await _0x2b55a0("*Example - :* .pair +923096287432");
+      return await _0x2b55a0("*Example - :* .pair +263719647303");
     }
     const _0x29763b = await fetchJson("https://silent-sobx-md-web-pair.onrender.com/code?number=" + _0x560654);
     const _0x4b5713 = _0x29763b.code;
-    _0xefafe8.reply(_0x4b5713 + "\n\n" + "_*SILENT-SOBX-MD NUMBER PAIRED SUCCESSFULLY...✅*_");
+    _0xefafe8.reply(_0x4b5713 + "\n\n" + "_*Here Is Your SubZero MD pair code...✅*_");
   } catch (_0x5be2b9) {
     console.log(_0x5be2b9);
     _0x2b55a0(_0x5be2b9);
