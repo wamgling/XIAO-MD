5c22239077331792fc0884f9c1649d07490e4009
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-31 18:07:07 +0200

    Update subzero-mf.js

--- plugins/subzero-mf.js
+++ plugins/subzero-mf.js
@@ -7,38 +7,37 @@ cmd({
     react: "🦄",
     filename: __filename
 }, async (conn, mek, m, { reply }) => {
-    const familyList = `*•────────────•⟢*
-       *[ • Developer: Darrell M ]*
-       *•────────────•⟢*
+    const familyList = `*⟣────────────⟢*
+    *[ • Developer: Darrell M ]*
+    *⟣────────────•⟢*
               
-      *╭┈───────────────•*
-      *│*  ◦🗂️ *REPOSITORY*
-      *│*   
-      *╵* https://github.com/mrfrank-ofc/SUBZERO-MD
-      *╰╶╶╶╶╶✲*\n
-      *╭┈───────────────•*
-      *│*  ◦🔗 *PROJECT NAME*
-      *│* SUBZERO-MD W.A BOT
-      *╰╶╶╶╶╶✲*\n
-      *╭┈───────────────•*
-      *│*  ◦👨‍💻 *DEVELOPER*
-      *│* https://github.com/mrfrank-ofc
-       *╰╶╶╶╶╶✲*\n
-      *╭┈───────────────•*
-      *│*  ◦🧮 *RELEASE DATE*
-      *│* 15 December 2024 
-       *╰╶╶╶╶╶✲*\n
-      *╭┈───────────────•*
-      *│*  ◦📩 *SUPPORT GROUP* 
-      *│* https://chat.whatsapp.com/InsR5qk3cBsG2781A6uxcO
-      *╰╶╶╶╶╶✲*\n
-      *╭┈───────────────•*
-      *│*  ◦🎀 *SUPPORT CHANNEL*
-      *│* https://whatsapp.com/channel/0029VagQEmB002T7MWo3Sj1D
-      *│*  
-      *│*  ◦🪄  Hit Me Here :wa.me/263719647303* 
-      *╰┈───────────────•*
-        *•────────────•⟢*
+      *⟣┈───────────────⟢*
+            🗂️ *REPOSITORY*
+       https://github.com/mrfrank-ofc/SUBZERO-MD
+      
+      *⟣┈───────────────•*
+            🔗 *PROJECT NAME*
+           SUBZERO-MD W.A BOT
+      
+      *⟣┈───────────────•*
+             👨‍💻 *DEVELOPER*
+     https://github.com/mrfrank-ofc
+       
+      *⟣┈───────────────•*
+             🧮 *RELEASE DATE*
+            15 December 2024 
+       
+      *⟣┈───────────────•*
+            📩 *SUPPORT GROUP* 
+      https://chat.whatsapp.com/InsR5qk3cBsG2781A6uxcO
+    
+      *⟣┈───────────────•*
+           🎀 *SUPPORT CHANNEL*
+      https://whatsapp.com/channel/0029VagQEmB002T7MWo3Sj1D
+        
+        ◦🪄  Hit Me Here :wa.me/263719647303* 
+      *⟝┈───────────────⟞*
+        *⟣────────────•⟢*
     `;
 
     try {
