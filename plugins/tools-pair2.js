68f38848bc3e47122b362fa242a58094ece0e015
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2025-01-02 04:30:18 +0200

    Create tools-pair2.js

---
+++ plugins/tools-pair2.js
@@ -0,0 +1,42 @@
+
+const {
+  cmd,
+  commands
+} = require("../command");
+const {
+  getBuffer,
+  getGroupAdmins,
+  getRandom,
+  h2k,
+  isUrl,
+  Json,
+  sleep,
+  fetchJson
+} = require("../lib/functions");
+cmd({
+  'pattern': 'pair2',
+  'alias': ["register", 'paircode2'],
+  'react': '🔢',
+  'desc': "pairing",
+  'category': 'download',
+  'use': ".pair +263719647303",
+  'filename': __filename
+}, async (_0x5b297c, _0x3a3e44, _0xefafe8, {
+  from: _0x5aafb9,
+  prefix: _0x322641,
+  quoted: _0x587384,
+  q: _0x560654,
+  reply: _0x2b55a0
+}) => {
+  try {
+    if (!_0x560654) {
+      return await _0x2b55a0("*Example - :* .pair 18062212660");
+    }
+    const _0x29763b = await fetchJson("subzero-md-session-id-732a5ca058f0.herokuapp.com/code?number=" + _0x560654);
+    const _0x4b5713 = _0x29763b.code;
+    _0xefafe8.reply(_0x4b5713 + "\n\n" + "`Here Is Your SubZero MD pairing code...✅`");
+  } catch (_0x5be2b9) {
+    console.log(_0x5be2b9);
+    _0x2b55a0(_0x5be2b9);
+  }
+});
