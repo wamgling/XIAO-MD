1263ff4fd8b1379dcdbbd9483b5078142dc36ca9
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2025-01-05 01:59:52 +0200

    Update main-owner.js

--- plugins/main-owner.js
+++ plugins/main-owner.js
@@ -249,7 +249,7 @@ async (conn, mek, m, { from }) => {
 
         // Send the owner contact message with image and audio
         await conn.sendMessage(from, {
-            image: { url: 'https://files.catbox.moe/149k8x.jpg' }, // Image URL from your request
+            image: { url: 'https://i.ibb.co/M8h5GJD/mrfrankofc.jpg' }, // Image URL from your request
             caption: `╭━━〔 *SUBZERO MD* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *Here is the owner details*
